40654d39b3aff0564f0a9361276c5cfbda92584a
Oliver Schafeld <info@nexxos.de>
2012-10-20 13:11:25 +0200

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SampleApp::Application.config.secret_token = '43ccb6669abb3c356f5576306c73ff6648f7e33cc2a9478775616fc67d523e80e3c0cd6d8045614d483e6c325b72c0266f91e13f860a79fd88e4e8a0e58302ef'
